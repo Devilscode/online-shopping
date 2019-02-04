c0b1433ed00653a3916b5c7efa9e41a6fa0b1aa5
Devilscode <itsshivansh39@gmail.com>
2019-02-04 04:18:52 -0800

    successfully complteted the interface theme

---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,16 @@
+ $(function()
+		 {
+	 switch(menu)
+	 {
+	 case'home':
+		 $('#home').addClass("active");
+		 break;
+		 //fetch the jquery elemnt using seelctor
+	 case'About us':
+		 $('#about').addClass("active");
+		 break;
+	 case'Contact us':
+		 $('#contact').addClass("active");
+		 break;
+	 }
+		 })
